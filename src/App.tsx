8ee3ffff93b26d045e7fd52e02b58cd8d7417ab4
RidhoAnshoryDev <muhridhoanshory@gmail.com>
2022-04-30 12:38:46 +0800

    adding styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import Typography from './components/Typography';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Button variant='secondary' size='medium'>
+        Button
+      </Button>
+      <Typography variant='h1'>Hello World</Typography>
+      <Typography variant='h3'>Hello World</Typography>
     </div>
   );
 }
